f31e379b5f01bc6ab8b3985b1c617c03aab13dab
色Prisma <c.prisma24@gmail.com>
2024-10-25 22:27:58 +0800

    Update null_data_log.js

--- null_data_log,js/null_data_log,js/null_data_log.js
+++ null_data_log,js/null_data_log,js/null_data_log.js
@@ -1,7 +1,2 @@
 // And of course, you won't stop there would you? ;)
-
-// I wonder how much lower of a human species can you be ?
-
-// Imagine a person so dedicated that they have to search up someone's github and search for their desired files inside their repositories?
-
-// Yup, that's you! YOU being so invested in this makes it more interesting to locate where you could possibly be rn.
+// I wonder how much lower of a human species can you be?
